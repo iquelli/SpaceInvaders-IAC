ebd3fa36debd5bf11be4422faa6bdebb2dd81f6f
Gocho1234 <goncalo.barias@gmail.com>
2022-05-22 16:28:34 +0100

    added the layout for the rest of the keyboard functions

--- grupo47.asm
+++ grupo47.asm
@@ -18,22 +18,25 @@ TRUE       EQU 0001H
 FALSE      EQU 0000H
 NULL       EQU 0000H
 
+PLACE 1000H
+
 ;=================================================================
 ; VARIABLE DECLARATION
 ;-----------------------------------------------------------------
 
 KEY_PRESSED:  WORD NULL
-KEY_PRESSING: WORD NULL
+KEY_CONVERTED: WORD NULL
 
 ;=================================================================
 ; THE CODE
 ;-----------------------------------------------------------------
 
+PLACE 0000H
+
 key_Handling:
 	CALL key_Sweeper
 	CALL key_Convert
-	CALL key_Update
-	CALL key_Actions
+	CALL key_CheckUpdate
 	RET
 
 key_Sweeper:
@@ -53,8 +56,8 @@ key_Sweeper_Wait:
 	MOVB  R3, [R1]
 	MOV   R1,  000FH
 	AND   R3,  R1
-	CMP   R3,  NULL
 	MOV   R1,  KEYPAD_COL
+	CMP   R3,  NULL
 	JZ    key_Sweeper_Wait
 
 key_Sweeper_Save:
@@ -70,17 +73,69 @@ key_Sweeper_Save:
 	RET
 
 key_Convert:
+	PUSH R0
+	PUSH R1
+	PUSH R2
+	PUSH R3
+
+	MOV  R2,  KEY_PRESSED
+	MOVB R0, [R2]
+	SHR  R2,  0004H
+	MOVB R1, [R2]
+
+	MOV  R2,  0000H
+	MOV  R3,  0000H
 
 key_Convert_Lin:
+	SHR  R0,  1
+	JZ   key_Convert_Col
+	ADD  R2,  0001H
+	JMP  key_Convert_Lin
 
 key_Convert_Col:
+	SHR  R1,  1
+	JZ   key_Convert_Return
+	ADD  R3,  0001H
+	JMP  key_Convert_Col
 
-Key_Convert_Return:
+key_Convert_Return:
+	MOV  R1,  0004H
+	MUL  R2,  R1
+	ADD  R2,  R3
+	MOV  R1,  KEY_CONVERTED
+	MOV [R1], R2
+
+	POP  R3
+	POP  R2
+	POP  R1
+	POP  R0
 	RET
 
 key_CheckUpdate:
+	PUSH R0
+	PUSH R1
+	PUSH R2
+	PUSH R3
+
+	MOV  R2,  KEY_PRESSED
+	MOVB R0, [R2]
+	MOV  R2,  KEYPAD_LIN
+	MOV  R3,  KEYPAD_COL
 
 key_CheckUpdate_Return:
+	CALL key_Actions
+	MOVB [R2], R0
+	MOVB  R1, [R3]
+	MOV   R3,  000FH
+	AND   R1,  R3
+	MOV   R3,  KEYPAD_COL
+	CMP   R1,  NULL
+	JNZ   key_CheckUpdate_Return
+
+	POP  R3
+	POP  R2
+	POP  R1
+	POP  R0
 	RET
 
 key_Actions:
